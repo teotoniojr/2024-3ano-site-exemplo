b759b15857c2525143abb5e608a3c785303bdd5a
Teotonio José de Souza Junior <102707827+teotoniojr@users.noreply.github.com>
2024-02-17 14:50:13 -0300

    Alterando para cores próprias e objetivos próprios

--- main.js
+++ main.js
@@ -17,9 +17,9 @@ for(let i=0; i<botoes.length; i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-02-20T08:00:00");
-const tempoObjetivo2 = new Date("2024-03-22T04:00:00");
-const tempoObjetivo3 = new Date("2024-11-05T12:00:00");
-const tempoObjetivo4 = new Date("2025-06-06T22:00:00");
+const tempoObjetivo2 = new Date("2024-03-12T08:00:00");
+const tempoObjetivo3 = new Date("2024-07-05T16:45:00");
+const tempoObjetivo4 = new Date("2025-07-05T22:00:00");
 
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
